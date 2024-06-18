7f9643181b45617a15bb05cb556e92a74f94f3fa
vikram kumar <vikramkumar8t8@gmail.com>
2024-06-19 00:54:41 +0500

    'first'

---
+++ app/components/Shekel.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Shekel = () => {
+  return (
+    <div className='bg-black w-full'>
+        <div className='max-w-[1920px] mx-auto w-full py-[212px] px-[100px]'>
+            <div className='bg-[#141515] bg-opacity-60 rounded-[20px] px-20 max-w-[1720px]'>
+                <h2 className='text-custom-color max-w-[1177px] mx-auto text-[85px] leading-[95px] tracking-[0.01em] text-center py-[72px]'>לשכפל את עצמך פי 10 מבלי להוציא שקל על משכורות ופרילנסרים!</h2>
+            </div>
+            <div className='text-white text-center max-w-[950px] mx-auto text-[40px] font-bold leading-[49px] tracking-[0.04em] mt-[63px] mb-[110px]'>
+                <h4 className='pb-8 '>מהפכת הבינה המלאכותית מאפשרת לאנשים שלא מפחדים ללמוד דברים חדשים להתפתח בקצב של עד פי 10 מאנשים רגילים ולייצר תוצאות מדהימות.</h4>
+                <h4 className='pb-8'>נכון, ההתחלה קצת קשה, ולא תמיד ברור לנו איך זה בכלל יכול לעזור לנו?!</h4>
+                <h4></h4>
+                <h4 className='pb-8'>ואין ספק שלהכיר אינסוף כלים חדשים שצצים כל יום כמו פטריות אחרי הגשם - יכול להרתיע.</h4>
+                <h4>אז איך בכל זאת אפשר למנף את היכולות של בינה מלאכותית בעסק, מבלי לעשות תואר בכל כלי חדש שיוצא, ומבלי שיווצר פער שישאיר אותך מאחור? </h4>
+            </div>
+            <div className='text-white text-center max-w-[950px] mx-auto text-[59px] font-bold leading-[82px] tracking-[0.04em] mt-[63px] mb-[167px]'>
+                <h5 className='text-[40px] pb-4'>או במלים אחרות:</h5>
+                <h4 className='pb-8'>איך להינות מעצמתם של כלי ה- AI החדשים בקלות ובנוחות, ולהכפיל את העסק שלך!</h4>
+                <h4>מבלי להוציא הון עתק על עובדים ופרילנסרים.</h4>
+            </div>
+            <div className='max-w-[1148px] mx-auto border-[3px] rounded-[33px] border-dashed border-[#FF5E5E] '>
+                <h2 className='text-[60px] font-bold leading-[95px] tracking-[0.01em] text-center text-[#FFFFFF] max-w-[600px] mx-auto py-[50px]'>אבל למה לקרוא על זה כשאפשר לנסות את זה..</h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Shekel
